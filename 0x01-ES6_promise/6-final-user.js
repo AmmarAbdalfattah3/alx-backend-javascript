ed09f2f1548e64c344fd4390dd2cd1becdd56676
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-07-24 21:58:42 +0000

    final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,13 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled(signUpUser(firstName, lastName), uploadPhoto(fileName))
-    .then((array) => {
-      return array.map((obj) => {
-        if (obj.status == 'fulfilled' ) {
-          return { status: 'fulfilled', value: obj.value }
-	}
-	else {
-	  return { status: 'rejected', reason: String(obj.reason) }
-	}
-
-      })
-    });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => (
+      results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      }))
+    ));
 }
