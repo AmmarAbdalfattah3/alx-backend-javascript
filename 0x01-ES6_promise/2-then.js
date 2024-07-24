25412e1bcf9939bcba836cba7698a8bd6994cb2a
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-07-24 09:12:16 +0000

    then

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default handleResponseFromAPI(promise) {
+  return  promise
+    .then(() => {
+      return ({ status: 200, body: 'success' });
+    })
+    .catch(() => {
+      return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
