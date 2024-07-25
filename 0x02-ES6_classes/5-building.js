83635f6394328e4af6dcb8698f3af8bb76de8fbc
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-07-25 22:36:02 +0000

    building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,20 @@
-class Building {
-    constructor(sqft) {
-        if (new.target === Building) {
-            throw new TypeError('Cannot construct Building instances directly');
-        }
-        this._sqft = sqft;
+export default class Building {
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new TypeError('Cannot construct Building instances directly');
     }
+    this._sqft = sqft;
 
-    get sqft() {
-        return this._sqft;
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+  }
 
-    evacuationWarningMessage() {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+  get sqft() {
+    return this._sqft;
+  }
+
+  static evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage);
+  }
 }
-
-export default Building;
