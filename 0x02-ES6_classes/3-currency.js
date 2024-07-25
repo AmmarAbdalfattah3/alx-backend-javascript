f1082a11b453b3a513522c833a1c0f9afe3b4ae4
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-07-25 15:08:42 +0000

    currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -7,17 +7,18 @@ export default class Currency {
   get code() {
     return this._code;
   }
-  
+
   set code(val) {
     if (typeof val !== 'string') {
       throw new TypeError('code must be a string');
     }
+    this._code = val;
   }
-  
+
   get name() {
     return this._name;
   }
-  
+
   set name(val) {
     if (typeof val !== 'string') {
       throw new TypeError('Name must be a string');
