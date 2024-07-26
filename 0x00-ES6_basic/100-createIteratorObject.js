9eb1330318c0456ed2a2f9dd40805aeb0948ed1d
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-07-26 18:38:45 +0000

    create iterator object

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,10 @@
 export default function createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
-  for (const department in allEmployees) {
-    if (allEmployees.hasOwnProperty(department)) {
-      for (const employee of allEmployees[department]) {
+  function* employeeGenerator() {
+    for (const department of Object.values(report)) {
+      for (const employee of department) {
         yield employee;
       }
     }
   }
+  return employeeGenerator();
 }
