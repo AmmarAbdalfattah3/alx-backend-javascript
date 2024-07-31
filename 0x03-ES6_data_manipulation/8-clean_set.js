33a8ac8c31ccc25625dc59b10f66fe1a3433814d
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-07-31 01:12:38 +0000

    filter set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (typeof startString !== 'string' || startString === '') {
+  if (typeof startString !== 'string' || startString) {
     return '';
   }
 
   const setValues = [];
-
-  for (const element of Array.from(set)) {
+  const array = Array.from(set);
+  for (const element of array) {
     if (element.startsWith(startString)) {
       setValues.push(element.slice(startString.length));
     }
