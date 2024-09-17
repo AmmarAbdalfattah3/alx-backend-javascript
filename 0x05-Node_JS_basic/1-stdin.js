53199fa62dba291b15e277d813778a2f8c183d4e
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-17 23:06:54 +0000

    console

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,13 +3,13 @@ console.log("Welcome to Holberton School, what is your name?");
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (input) => {
-    const name = input.trim();
+  const name = input.trim();
     
-    console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}`);
 
-    process.exit();
+  process.exit();
 });
 
 process.on('exit', () => {
-    console.log("This important software is now closing");
+  console.log("This important software is now closing");
 });
