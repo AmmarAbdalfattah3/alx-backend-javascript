f6577f3fe1a11227f3ed4607fe6c69b70c9bc3a3
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-18 20:05:10 +0000

    http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,31 +2,27 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-
   if (req.url === '/') {
-    res.statusCode = 200;
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.statusCode = 200;
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
 
     countStudents(process.argv[2])
-      .then(() => {
+      .then((data) => {
+        res.write(data);
         res.end();
       })
-      .catch((error) => {
-        res.statusCode = 500;
-        res.end(`Error: ${error.message}`);
+      .catch((err) => {
+        res.end(err.message);
       });
   } else {
-    res.statusCode = 404;
-    res.end('Not Found');
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Resource not found');
   }
 });
 
-app.listen(1245, () => {
-  console.log('Server listening on port 1245');
-});
+app.listen(1245);
 
 module.exports = app;
--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,31 +1,13 @@
 const express = require('express');
 
-// Create an Express application
 const app = express();
 
-// Define a route for the root path
 app.get('/', (req, res) => {
   res.status(200).send('Hello Holberton School!');
 });
 
-// Handle other routes with a 404 Not Found error
-app.use((req, res) => {
-  res.status(404).send(`<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Error</title>
-</head>
-<body>
-<pre>Cannot GET ${req.originalUrl}</pre>
-</body>
-</html>`);
-});
-
-// Make the server listen on port 1245
 app.listen(1245, () => {
   console.log('Server is listening on port 1245');
 });
 
-// Export the Express application
 module.exports = app;
