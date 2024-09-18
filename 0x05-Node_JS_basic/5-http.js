1100301848600fdd73ef571fd20a953b3db295f0
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-18 21:59:13 +0000

    http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,29 +1,31 @@
+const url = require('url');
 const http = require('http');
 const countStudents = require('./3-read_file_async');
-const url = require('url');
 
 const app = http.createServer(async (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    const path = url.parse(req.url).pathname;
+  const path = url.parse(req.url).pathname;
 
-    if (path === '/') {
-        res.end('Hello Holberton School!');
-    } else if (path === '/students') {
-        res.write('This is the list of our students\n');
-        try {
-            await countStudents(process.argv[2]);
-        } catch (error) {
-            res.end(error.message);
-        }
-    } else {
-        res.writeHead(404);
-        res.end('Not found');
+  if (path === '/') {
+    res.end('Hello Holberton School!');
+  } else if (path === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      await countStudents(process.argv[2]);
+    } catch (error) {
+      res.end(error.message);
+      return; // Ensure we exit after handling the error
     }
+    res.end(); // End the response after successfully processing
+  } else {
+    res.writeHead(404);
+    res.end('Not found');
+  }
 });
 
 app.listen(1245, () => {
-    console.log('Server is listening on port 1245');
+  console.log('Server is listening on port 1245');
 });
 
 module.exports = app;
