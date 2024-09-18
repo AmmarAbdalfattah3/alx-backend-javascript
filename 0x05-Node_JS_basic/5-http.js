157a1c1c8c8ee59d97ba67c4dc52521a958c00dc
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-18 21:42:03 +0000

    http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,14 +1,18 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async'); // Importing the countStudents function
 
 const app = http.createServer((req, res) => {
   const { url } = req;
 
   if (url === '/') {
+    // Handle the root endpoint
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!\n');
   } else if (url === '/students') {
+    // Handle the /students endpoint
     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    
+    // Call the countStudents function
     countStudents(process.argv[2])
       .then(() => {
         res.end('This is the list of our students\n');
@@ -18,6 +22,7 @@ const app = http.createServer((req, res) => {
         res.end(`${error.message}\n`);
       });
   } else {
+    // Handle 404 for any other routes
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found\n');
   }
@@ -25,6 +30,7 @@ const app = http.createServer((req, res) => {
 
 const PORT = 1245;
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
