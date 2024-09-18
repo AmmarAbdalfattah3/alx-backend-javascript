f29c53efcf11f4fd2c7d27e32e2f7cf7d2cfbe59
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-18 21:37:56 +0000

    http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,25 +1,23 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer(async (req, res) => {
-  const { method, url } = req;
+const app = http.createServer((req, res) => {
+  const { url } = req;
 
   if (url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!\n');
-  } 
-  else if (url === '/students') {
+  } else if (url === '/students') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    
-    try {
-      await countStudents(process.argv[2]);
-      res.end('This is the list of our students\n');
-    } catch (error) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end(`${error.message}\n`);
-    }
-  }
-  else {
+    countStudents(process.argv[2])
+      .then(() => {
+        res.end('This is the list of our students\n');
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(`${error.message}\n`);
+      });
+  } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found\n');
   }
