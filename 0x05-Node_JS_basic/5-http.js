2629869c6bd52fea24920db5c437097e57836732
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-18 23:16:36 +0000

    http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,41 +1,31 @@
-const http = require('http');
 const url = require('url');
-const countStudents = require('./3-read_file_async');  // Import the countStudents function
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
-// Create the server
 const app = http.createServer(async (req, res) => {
-  const parsedUrl = url.parse(req.url, true);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  // Handle the root route "/"
-  if (parsedUrl.pathname === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+  const path = url.parse(req.url).pathname;
+
+  if (path === '/') {
     res.end('Hello Holberton School!');
-  }
-
-  // Handle the "/students" route
-  else if (parsedUrl.pathname === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+  } else if (path === '/students') {
     res.write('This is the list of our students\n');
-
     try {
-      // Pass the CSV file from the command line argument
-      const result = await countStudents(process.argv[2]);
-      res.end(result);  // Send the result from countStudents to the client
-    } catch (err) {
-      res.end(err.message);  // Send error message if the file cannot be loaded
+      await countStudents(process.argv[2]);
+    } catch (error) {
+      res.end(error.message);
+      return;
     }
-  }
-
-  // Handle unknown routes
-  else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end();
+  } else {
+    res.writeHead(404);
     res.end('Not found');
   }
 });
 
-// The server listens on port 1245
 app.listen(1245, () => {
-  console.log('Server is running on port 1245');
+  console.log('Server is listening on port 1245');
 });
 
 module.exports = app;
