78db7c8720214e569c7930882dda8336d4cf2b74
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-18 01:43:36 +0000

    http express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Define a route for the root path
+app.get('/', (req, res) => {
+  res.status(200).send('Hello Holberton School!');
+});
+
+// Handle other routes with a 404 Not Found error
+app.use((req, res) => {
+  res.status(404).send('<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="utf-8">\n<title>Error</title>\n</head>\n<body>\n<pre>Cannot GET ' + req.originalUrl + '</pre>\n</body>\n</html>');
+});
+
+// Make the server listen on port 1245
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+// Export the Express application
+module.exports = app;
