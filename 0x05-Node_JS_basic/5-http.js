22c1a064edb562a7fb0a8e49689ae97784e8b2ae
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-18 21:50:54 +0000

    http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,38 +1,29 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async'); // Importing the countStudents function
+const countStudents = require('./3-read_file_async');
+const url = require('url');
 
-const app = http.createServer((req, res) => {
-  const { url } = req;
+const app = http.createServer(async (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  if (url === '/') {
-    // Handle the root endpoint
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!\n');
-  } else if (url === '/students') {
-    // Handle the /students endpoint
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    
-    // Call the countStudents function
-    countStudents(process.argv[2])
-      .then(() => {
-        res.end('This is the list of our students\n');
-      })
-      .catch((error) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(`${error.message}\n`);
-      });
-  } else {
-    // Handle 404 for any other routes
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found\n');
-  }
+    const path = url.parse(req.url).pathname;
+
+    if (path === '/') {
+        res.end('Hello Holberton School!');
+    } else if (path === '/students') {
+        res.write('This is the list of our students\n');
+        try {
+            await countStudents(process.argv[2]);
+        } catch (error) {
+            res.end(error.message);
+        }
+    } else {
+        res.writeHead(404);
+        res.end('Not found');
+    }
 });
 
-const PORT = 1245;
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.listen(1245, () => {
+    console.log('Server is listening on port 1245');
 });
 
 module.exports = app;
