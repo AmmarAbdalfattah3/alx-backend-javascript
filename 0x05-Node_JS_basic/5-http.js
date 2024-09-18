a73cfe71118ae2682b029d6e151f91caacb6a135
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-18 01:41:35 +0000

    http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -12,26 +12,31 @@ const app = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    // Handle /students path
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.write('This is the list of our students\n');
 
+    // Read the database file asynchronously and send the student data
     countStudents(databaseFile)
       .then(() => {
-        res.end();
+        res.end(); // End the response once the data has been logged
       })
       .catch((err) => {
-        res.end(err.message);
+        res.end(err.message); // Send error message if the database cannot be loaded
       });
   } else {
+    // Handle other paths (404 Not Found)
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Not Found');
   }
 });
 
+// Listen on port 1245
 app.listen(1245, () => {
   console.log('Server is listening on port 1245');
 });
 
+// Export the app
 module.exports = app;
