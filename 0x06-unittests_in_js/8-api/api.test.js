3fafca13f9dc5f3303fffcdf123f88df78109328
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-20 16:39:01 +0000

    api test

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const { expect } = require('chai');
+const app = require('./api');
+
+describe('Index page', () => {
+  it('Correct status code', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Other - check content type', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.headers['content-type']).to.contain('text/html');
+      done();
+    });
+  });
+});
